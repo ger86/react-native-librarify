48edc1196e766956602bf7aa9ca407cb443aab26
Gerardo Fernández <gefe.86@gmail.com>
2020-05-24 18:28:03 +0200

    feature: fetch books from API

--- src/components/Book/BookListItem.js
+++ src/components/Book/BookListItem.js
@@ -1,10 +1,35 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-ionicons';
 
-export default function BookListItem({book}) {
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    paddingVertical: 15,
+    paddingLeft: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: '#E0E0E0',
+  },
+  image: {
+    width: 50,
+    height: 50,
+    marginRight: 10,
+  },
+  icon: {
+    color: '#000',
+    marginLeft: 'auto',
+  },
+});
+
+const IMAGE_URL = 'https://picsum.photos/200/75';
+export default function BookListItem({book, onPress}) {
   return (
-    <View>
-      <Text>{book.title}</Text>
-    </View>
+    <TouchableOpacity onPress={onPress}>
+      <View style={styles.container}>
+        <Image source={{uri: IMAGE_URL}} style={styles.image} />
+        <Text>{book.title}</Text>
+        <Icon name="chevron-forward-outline" style={styles.icon} />
+      </View>
+    </TouchableOpacity>
   );
 }
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,33 +1,38 @@
 import React from 'react';
-import {FlatList, Text, TouchableHighlight, View} from 'react-native';
+import {useQuery} from 'react-query';
+import {FlatList, Text, View} from 'react-native';
 import BookListItem from '../components/Book/BookListItem';
 
-const BOOK_LIST = [
-  {
-    id: 1,
-    title: 'Hacia rutas salvajes',
-  },
-  {
-    id: 2,
-    title: 'El nombre del viento',
-  },
-  {
-    id: 3,
-    title: 'Ensaño sobre la ceguera',
-  },
-];
+const GET_BOOKS = 'GET_BOOKS';
+
+async function fetchData() {
+  const response = await fetch('http://127.0.0.1:8000/books');
+  const json = await response.json();
+  return json;
+}
 
 export default function Home({navigation}) {
+  function handleOnPress() {
+    navigation.navigate('Library');
+  }
+
+  const {status, data} = useQuery(GET_BOOKS, fetchData);
+
   return (
     <View>
       <FlatList
-        data={BOOK_LIST}
-        renderItem={({item}) => <BookListItem book={item} />}
+        data={status === 'success' ? data.data : []}
+        renderItem={({item}) => (
+          <BookListItem book={item} onPress={handleOnPress} />
+        )}
         keyExtractor={item => item.id}
         ListHeaderComponent={
           <View>
             <Text>Mi lista de libros</Text>
           </View>
+        }
+        ListEmptyComponent={
+          <View>{status === 'loading' && <Text>Cargando libros...</Text>}</View>
         }
       />
     </View>
